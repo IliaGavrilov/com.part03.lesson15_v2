aff6241cbf68eb4432303ebec93773fe824ce655
Ilia Gavrilov <elijahgavri@gmail.com>
2019-05-27 21:46:33 +0300

    connection to DB established

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        //String url = "jdbc:postgresql://localhost:5432/postgres";
-        String url = "jdbc:oracle:thin:@//localhost:5432:postgres";
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+
+        //String url = "jdbc:oracle:thin:@//localhost:5432:postgres";
+        String user = "postgres";
+        //String password = "postgres";
+        String password = "jU3y3gIV";
 
         /* Connection - объект, который держит соединение с БД */
         try {
-            Connection cn =
-                    DriverManager.getConnection(url,
-                    "postgres", "jU3y3gIV");
-
+            //Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/example", "postgres", "postgres");
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            Connection cn = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
